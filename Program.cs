4a6966ad2d1767e49659b9f83eb32235458d893c
Vladislav <vladislav.v.o@yandex.ru>
2022-06-26 18:18:04 +0300

    решены 3 задачи

--- Program.cs
+++ Program.cs
@@ -9,6 +9,34 @@
 918 -> 1
 */
 
+/*
+    Console.Write("Input number ");
+    int number1 = Convert.ToInt32(Console.ReadLine());
+
+int FindSecondNumber(int num1)
+{
+
+    int res = num1 % 2;
+    return res;
+}
+
+int result = FindSecondNumber();
+
+Console.WriteLine("The second digit of " + number1 + " is " + result);
+
+*/
+
+/**********************************************************************************
+Console.Write("Input number ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int result = (number1 % 100) / 10;
+
+Console.WriteLine("The second digit of " + number1 + " is " + result);
+
+***********************************************************************************/
+
+
 
 /*
 ## Задача 13: 
@@ -24,7 +52,38 @@
 */
 
 
+
+Console.Write("Input number ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+int digitCount = (int)Math.Log10(number1) + 1;
+int StepenDeleniya, result;
+
+
+if (digitCount < 3)
+
+        {
+            Console.WriteLine("третьей цифры нет");
+        }
+
+        else
+
+        {
+            StepenDeleniya = Convert.ToInt32(Math.Pow(10, digitCount - 2));
+
+            result = number1 % StepenDeleniya / (StepenDeleniya / 10);
+
+            Console.WriteLine(result);
+        }
+
+
+
+
+
 /*
+int result = number1 % 10;
+
+
 
 ## Задача 15: 
 
@@ -36,3 +95,22 @@
 
 
 */
+
+/**********************************************************************************
+Console.Write("Input number of day ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 < 6) 
+{
+Console.WriteLine("нет");
+}
+else
+    if (number1 < 8) 
+    {
+    Console.WriteLine("да"); 
+    }
+    else
+    Console.WriteLine("ой"); 
+
+
+***********************************************************************************/
